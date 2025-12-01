29bb0f197e7ac26d1e57fad03f2d7b344a3517fe
Sean Kim <sean.kim428@gmail.com>
2025-12-01 20:40:35 +0900

    update board logic to protect member's private information

--- src/main/java/com/example/test/dto/BoardResponseDto.java
+++ src/main/java/com/example/test/dto/BoardResponseDto.java
@@ -9,14 +9,16 @@ public class BoardResponseDto {
     private Long id;
     private String title;
     private String content;
-    private String writer;
+    private String nickname;
+    private String username;
     private LocalDateTime createdAt;
 
     public BoardResponseDto(Board board) {
         this.id = board.getId();
         this.title = board.getTitle();
         this.content = board.getContent();
-        this.writer = board.getMember() != null ? board.getMember().getUsername() : "알 수 없음";
+        this.nickname = board.getMember() != null ? board.getMember().getNickname() : "알 수 없음";
+        this.username = board.getMember().getUsername();
         this.createdAt = board.getCreatedAt();
     }
 }
