bd38440c01777cae256aa004e1eaa8b20cb1f1c3
Sean Kim <sean.kim428@gmail.com>
2025-12-01 17:30:53 +0900

    update board logic to protect member's private information

--- src/main/java/com/example/test/service/BoardService.java
+++ src/main/java/com/example/test/service/BoardService.java
@@ -1,5 +1,6 @@
 package com.example.test.service;
 
+import com.example.test.dto.BoardResponseDto;
 import com.example.test.entity.Member;
 import com.example.test.repository.MemberRepository;
 import com.example.test.entity.Board;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +21,7 @@ public class BoardService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Board createBoard(String title, String content, String username) {
+    public BoardResponseDto createBoard(String title, String content, String username) {
 
         Member member = memberRepository.findByUsername(username)
                 .orElseThrow(() -> new RuntimeException("그런 사람 없다 회원가입부터 해 씨발 ㅋㅋ"));
@@ -28,10 +30,13 @@ public class BoardService {
                 .content(content)
                 .member(member)
                 .build();
-        return boardRepository.save(board);
+        return new BoardResponseDto(board);
     }
-    public List<Board> getAllBoards() {
-        return boardRepository.findAll();
+    //기존에는 List<Board>로 보드에 기입되어있는 모든 정보를 보냈음. 하지만 보안상 매우 좋지 않음
+    //따라서 Dto에 BoardResponseDto 클래스를 만들어, 멤버값의 닉네임만 꺼내와서 포장해서 보내도록 함.
+    public List<BoardResponseDto> getAllBoards() {
+        return boardRepository.findAll().stream()
+                .map(BoardResponseDto::new).collect(Collectors.toList());
     }
 
     @Transactional
@@ -44,14 +49,14 @@ public class BoardService {
         boardRepository.delete(board);
     }
     @Transactional
-    public Board updateBoard(Long id, String title, String content, String username) {
+    public BoardResponseDto updateBoard(Long id, String title, String content, String username) {
         Board board = boardRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("글이 없는데 수정 어케함?"));
         if (!board.getMember().getUsername().equals(username)) {
             throw new RuntimeException("니꺼나 수정해라 다른 글에 똥싸지르지 말고");
         }
         board.update(title, content);
-        return board;
+        return new BoardResponseDto(board);
     }
 }
 
